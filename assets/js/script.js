938c1c683a7683f3c8db3d0563da9f4c610afcbb
Trent_D <tdickson91@gmail.com>
2022-05-25 18:54:48 -0400

    quiz determines if user is correct or incorrect, however the quiz is missing the following items: leaderboards, initials and scoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
-let startBtn = document.querySelector(".start-button");
+let startBtn = document.querySelector("#start-button");
+let replayBtn = document.querySelector("#replay-button")
 let timeLeft = 45;
-let winCounter = 0;
-let lossCounter = 0;
+let score = 0;
 let currentQuestion = 0;
-
-
+let wins = 0;
+let losses = 0;
 
 // Make questions appear one by one 
 function generateQuiz() {
-    // What is the first thing?
-    // A question should appear with four answers to be chosen
-
+    // Question appears with four answers in buttons to be chosen
     let questionArray = questions[currentQuestion];
-
     // Display question one and the four answers
     let quizBox = document.body.querySelector('.quizBox');
         // Answer question one
@@ -24,13 +21,36 @@ function generateQuiz() {
         button.textContent = questionArray.options[i];
         quizBox.append(button);
         button.addEventListener('click', function () {
+            // If 
+            if (event.target.innerHTML === questionArray.answer) {
+                score++;
+                console.log(score);
+            } else {
+                console.log('Incorrect!');
+                timeLeft -=5;
+            } 
+
+            // then win condition 
+            if (currentQuestion === questions.length) {
+                var name = prompt('Enter your name', 'someName');
+                if (name != null && name != "") {
+                    alert("Your name is " + name + " and your score is " + score);
+                    document.getElementById('replay').style.display = "block";
+                }
+            }
+            // Lose condition = time reaches zero and a replay button appears
+            if (timeLeft <= 0) {
+                alert("You lose!");
+                document.getElementById('replay').style.display = "block";
+            }
+
             currentQuestion++;
             generateQuiz();
-            // When you click an answer, next question shows
-            // If you are wrong, then minus 5 seconds off timeLeft
         });
     }
 }
+
+
 
 // Timer interval
 function startTimer(time){
@@ -43,13 +63,14 @@ function startTimer(time){
         // DOM
     let timerSeconds = document.querySelector('.timerSeconds');
         timerSeconds.textContent = timeLeft;
-        console.log (timeLeft);
     }
 }
 
 function startQuiz() {
+    generateQuiz();
     startTimer();
-    generateQuiz();    
 }
 
 startBtn.addEventListener("click", startQuiz);
+// Replay button
+replayBtn.addEventListener("click", startQuiz);
