c9e455932bd8f974dd31a931f791df409804b07b
Trent_D <tdickson91@gmail.com>
2022-05-24 21:56:22 -0400

    an array of object arrays and key-value pairs

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Who created JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Brendan Eich",
+      "Mark Zuckerberg",
+      "Chauncy Billups",
+      "Cascading Style Sheets"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What integrated development environment (IDE) can you use to code JavaScript?",
+    answer: "All of the above",
+    options: [
+      "Visual Studio Code",
+      "Eclipse",
+      "IntelliJ",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What HTML element do we place the JavaScript file?",
+    answer: "script",
+    options: [
+      "script",
+      "scripted",
+      "JavaScript",
+      "js"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Where is the correct place to insert a JavaScript?",
+    answer: "Body",
+    options: [
+      "Title",
+      "Head",
+      "Body",
+      "Footer"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is JavaScript primarily used for?",
+    answer: "Front-end",
+    options: [
+      "Front-end",
+      "Back-end",
+      "Middle-end",
+      "None of the above"
+    ]
+    }
+]
